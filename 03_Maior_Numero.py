d1d31ab1d6bb3089928603fb6672c8e494a37f58
Cauê Bolzan <cauebolzan@gmail.com>
2025-08-02 20:58:19 -0400

    03_Maior_Numero.py
    Este programa solicita ao usuário que digite quatro números inteiros e, em seguida, determina qual deles é o maior. Ele compara cada número com o atual valor considerado como o maior e atualiza esse valor conforme necessário. Ao final, exibe o maior número digitado.

---
+++ 03_Maior_Numero.py
@@ -0,0 +1,13 @@
+n1 = float(input("Digite o primeiro numero"))
+n2 = float(input("Digite o segundo numero"))
+n3 = float(input("Digite o terceiro numero"))
+n4 = float(input("Digite o ultimo numero"))
+maior = n1 
+if maior < n2:
+  maior = n2 
+if maior < n3:
+  maior = n3
+if maior < n4:
+  maior = n4 
+
+print (maior)
