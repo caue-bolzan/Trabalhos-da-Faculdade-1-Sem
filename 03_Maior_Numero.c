8372013e28ded1ec1110a4a6dd9bd5cb518b3b93
Cauê Bolzan <cauebolzan@gmail.com>
2025-08-02 21:01:51 -0400

    03_Maior_Numero.c
    Este programa solicita ao usuário que digite quatro números inteiros e, em seguida, determina qual deles é o maior. Ele compara cada número com o atual valor considerado como o maior e atualiza esse valor conforme necessário. Ao final, exibe o maior número digitado.

--- 03_Maior_Numero.c
+++ 03_Maior_Numero.c
@@ -1,32 +1,32 @@
 #include <stdio.h> 
 int main (){
-  int n1;
-  int n2;
-  int n3;
-  int n4;
-  int maior;
+  float n1;
+  float n2;
+  float n3;
+  float n4;
+  float maior;
   
   printf("Digite o primeiro numero: \n");
-  scanf("%d", &n1);
+  scanf("%f", &n1);
   maior = n1;
   
   printf("Digite o segundo numero: \n");
-  scanf("%d", &n2);
+  scanf("%f", &n2);
   if (maior < n2){
-    maior = n2
+    maior = n2;
   }
   
   printf("Digite o terceiro numero: \n");
-  scanf("%d", &n3);
+  scanf("%f", &n3);
   if (maior < n3){
-    maior = n3
+    maior = n3;
   }
   
   printf("Digite o ultimo numero: \n");
-  scanf("%d", &n4);  
+  scanf("%f", &n4);  
   if (maior < n4){
-    maior = n4
+    maior = n4;
   }
-  printf("o seu maior numero é %d", maior);
+  printf("o seu maior numero é %f", maior);
   return 0;
 }
