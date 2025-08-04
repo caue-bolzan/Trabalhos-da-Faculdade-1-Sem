0945ebebabb52e08772f5dc15e16fb4330256269
Cauê Bolzan <cauebolzan@gmail.com>
2025-08-03 22:25:27 -0400

    04_Contagem_Gols.py
    Esse código em PYTHON solicita ao usuário a quantidade de gols marcados por dois times, compara os valores e exibe se houve empate, se o Time 1 ganhou ou se o Time 2 ganhou. É um programa simples de decisão usando estruturas condicionais if, elif e else.

---
+++ 04_Contagem_Gols.py
@@ -0,0 +1,10 @@
+gol1 = int(input("Quantos gols o primeiro time fez?"))
+gol2 = int(input("Quantos gols o segundo time fez?"))
+
+if gol1 == gol2:
+  print("Empate")
+elif gol1 > gol2:
+  print("Time 1 ganhou!")
+else:
+  print("Time 2 ganhou!")
+  
